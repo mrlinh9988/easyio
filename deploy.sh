b2f49fe33b24196d7c3235a8d04b3be34ad1b37b
mrlinh9988 <mrlinhdeptrai9988@gmail.com>
2021-02-15 22:32:18 +0700

    update

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 tar czf easyio.tar.gz main.js package.json public LICENSE
-scp easyio.tar.gz nanogram@nanogram.io:~
+scp easyio.tar.gz ubuntu@184.72.126.120:~
 rm easyio.tar.gz
 
-ssh nanogram@nanogram.io << 'ENDSSH'
+ssh ubuntu@184.72.126.120 << 'ENDSSH'
 pm2 stop all
 rm -rf easyio
 mkdir easyio
